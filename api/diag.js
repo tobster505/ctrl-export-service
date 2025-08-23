5015abd3a4aecd7c4d04ee146e65a36407b6f5e9
tobster505 <theneverendinglearner@gmail.com>
2025-08-23 17:50:05 +0200

    Update diag.js

--- api/diag.js
+++ api/diag.js
@@ -1,7 +1,4 @@
-// either delete this line completely...
-export const config = { runtime: 'nodejs' };   // ← if you keep it, it must be exactly 'nodejs'
-
-// ...rest of your diag code
+// /api/diag.js
 export default async function handler(req, res) {
   res.setHeader('content-type', 'application/json');
   res.end(JSON.stringify({ ok: true, runtime: 'nodejs' }));
